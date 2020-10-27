18c4b56e8e6b20b35e89d057cec01458fc2a61c7
AswadMirza <1mirzaasw@gmail.com>
2020-10-27 19:24:52 -0400

    Added Gun Object and Script

---
+++ Aswad_Mirza_Assignment1/Assets/Scripts/Pickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Container;
+    public float RotationSpeed = 80f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // roatates the object on its y axis
+        Container.transform.Rotate(Vector3.up* RotationSpeed*Time.deltaTime);
+    }
+}
