5c261f875b113346a8d43fefadb7694450963c4b
AswadMirza <1mirzaasw@gmail.com>
2020-10-28 17:10:55 -0400

    Added 6 animations to the player character, and a walking animation to the civilians
    Part 1 is finished. Next is part 2

---
+++ Aswad_Mirza_Assignment1/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterAnimation : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // if one is held down, set hip hop to true, else set it to false
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            anim.SetBool("HipHop", true);
+        }
+        else {
+            anim.SetBool("HipHop", false);
+        }
+
+        // if 2 is held down
+
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            anim.SetBool("House", true);
+        }
+        else
+        {
+            anim.SetBool("House", false);
+        }
+
+        // if 3 is held down
+
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            anim.SetBool("Robot", true);
+        }
+        else
+        {
+            anim.SetBool("Robot", false);
+        }
+
+        // if 4 is held down
+
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            anim.SetBool("Salsa", true);
+        }
+        else
+        {
+            anim.SetBool("Salsa", false);
+        }
+        // if 5 is held down
+        if (Input.GetKey(KeyCode.Alpha5))
+        {
+            anim.SetBool("Samba", true);
+        }
+        else
+        {
+            anim.SetBool("Samba", false);
+        }
+        // if 6 is held down
+        if (Input.GetKey(KeyCode.Alpha6))
+        {
+            anim.SetBool("Swing", true);
+        }
+        else
+        {
+            anim.SetBool("Swing", false);
+        }
+
+    }
+}
