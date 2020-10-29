6c3fa6b8f76497422b6e06ab0cd06175a3225ba8
AswadMirza <1mirzaasw@gmail.com>
2020-10-28 22:11:50 -0400

    Part 3 is finished
    Added gun pickups, now going to implement animations and utility. for part 4

---
+++ Aswad_Mirza_Assignment1/Assets/Scripts/PropManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Code taken from week 4 from slate. Based on code from "Unity 2018 Cookbook Third Edition" Chapter 10, exercise 6
+public class PropManager : MonoBehaviour
+{
+    public GameObject prop;
+    public Transform targetBone;
+    public Vector3 positionOffset;
+    public Vector3 rotationOffset;
+    public bool destroyTrigger = true;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        bool addPropCondition = targetBone.IsChildOf(collision.transform) & !AlreadyHalreadyHasChildObject();
+        if (addPropCondition)
+            AddProp();
+    }
+
+    private void AddProp()
+    {
+        GameObject newprop;
+        newprop = Instantiate(prop, targetBone.position,
+                  targetBone.rotation) as GameObject;
+        newprop.name = prop.name;
+        newprop.transform.parent = targetBone;
+        newprop.transform.localPosition = positionOffset;
+        newprop.transform.localEulerAngles = rotationOffset;
+        if (destroyTrigger)
+            Destroy(gameObject);
+    }
+
+    private bool AlreadyHalreadyHasChildObject()
+    {
+        string propName = prop.name;
+        foreach (Transform child in targetBone)
+        {
+            if (child.name == propName)
+                return true;
+        }
+        return false;
+    }
+}
